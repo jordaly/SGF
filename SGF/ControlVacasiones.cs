fb56c3f84df9d99c484aa095c3a4389d8b10c7b4
Jordaly José Suriel Vargas <38952588+jordaly@users.noreply.github.com>
2020-11-30 17:39:56 -0400

    formulario de empleados
    agrege el formulario de empleados, el de control de pagos, y el de vacaciones.
    pero el de vacaciones todavia no esta terminado

---
+++ SGF/ControlVacasiones.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SGF
+{
+    public partial class ControlVacasiones : FormProcesos
+    {
+        public ControlVacasiones()
+        {
+            InitializeComponent();
+        }
+        public override void Nuevo()
+        {
+            //RegistroEmpleados rc = new RegistroEmpleados();
+            //rc.ShowDialog();
+
+
+            //refrescarDatos(BuscarDatos);
+            //cmd = "";
+            //MessageBox.Show(cmd);
+            //rtbxIndicaciones.Text = cmd;
+            //Console.Out(cmd);
+            //ds = Utilidades.EjecutarDS(cmd);
+            DateTime hoy = DateTime.Now;
+            DateTime FechaFinal = Convert.ToDateTime("1/11/2020");
+            TimeSpan t = FechaFinal - hoy;
+            double NrOfDays = t.TotalDays;
+            MessageBox.Show(""+NrOfDays);
+        }
+    }
+}
--- SGF/FormProcesos.Designer.cs
+++ SGF/FormProcesos.Designer.cs
@@ -29,13 +29,13 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxBuscar = new System.Windows.Forms.ComboBox();
             this.btnBuscar = new System.Windows.Forms.Button();
             this.btnBorrar = new System.Windows.Forms.Button();
             this.btnNuevo = new System.Windows.Forms.Button();
             this.btnModificar = new System.Windows.Forms.Button();
             this.dgvPadre = new System.Windows.Forms.DataGridView();
-            this.cbxBuscar = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.ds)).BeginInit();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPadre)).BeginInit();
@@ -51,15 +51,37 @@
             this.panel1.Controls.Add(this.btnModificar);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(800, 67);
+            this.panel1.Size = new System.Drawing.Size(1067, 82);
             this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(455, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Buscar por:";
+            // 
+            // cbxBuscar
+            // 
+            this.cbxBuscar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxBuscar.FormattingEnabled = true;
+            this.cbxBuscar.Location = new System.Drawing.Point(455, 39);
+            this.cbxBuscar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbxBuscar.Name = "cbxBuscar";
+            this.cbxBuscar.Size = new System.Drawing.Size(160, 24);
+            this.cbxBuscar.TabIndex = 4;
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(245, 11);
+            this.btnBuscar.Location = new System.Drawing.Point(327, 14);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(72, 42);
+            this.btnBuscar.Size = new System.Drawing.Size(96, 52);
             this.btnBuscar.TabIndex = 3;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
@@ -67,9 +89,10 @@
             // 
             // btnBorrar
             // 
-            this.btnBorrar.Location = new System.Drawing.Point(167, 11);
+            this.btnBorrar.Location = new System.Drawing.Point(223, 14);
+            this.btnBorrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnBorrar.Name = "btnBorrar";
-            this.btnBorrar.Size = new System.Drawing.Size(72, 42);
+            this.btnBorrar.Size = new System.Drawing.Size(96, 52);
             this.btnBorrar.TabIndex = 2;
             this.btnBorrar.Text = "Borrar";
             this.btnBorrar.UseVisualStyleBackColor = true;
@@ -77,9 +100,10 @@
             // 
             // btnNuevo
             // 
-            this.btnNuevo.Location = new System.Drawing.Point(89, 11);
+            this.btnNuevo.Location = new System.Drawing.Point(119, 14);
+            this.btnNuevo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnNuevo.Name = "btnNuevo";
-            this.btnNuevo.Size = new System.Drawing.Size(72, 42);
+            this.btnNuevo.Size = new System.Drawing.Size(96, 52);
             this.btnNuevo.TabIndex = 1;
             this.btnNuevo.Text = "Nuevo";
             this.btnNuevo.UseVisualStyleBackColor = true;
@@ -87,9 +111,10 @@
             // 
             // btnModificar
             // 
-            this.btnModificar.Location = new System.Drawing.Point(11, 11);
+            this.btnModificar.Location = new System.Drawing.Point(15, 14);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(72, 42);
+            this.btnModificar.Size = new System.Drawing.Size(96, 52);
             this.btnModificar.TabIndex = 0;
             this.btnModificar.Text = "Modificar";
             this.btnModificar.UseVisualStyleBackColor = true;
@@ -104,38 +129,23 @@
             this.dgvPadre.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dgvPadre.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvPadre.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dgvPadre.Location = new System.Drawing.Point(0, 67);
+            this.dgvPadre.Location = new System.Drawing.Point(0, 82);
+            this.dgvPadre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dgvPadre.Name = "dgvPadre";
             this.dgvPadre.ReadOnly = true;
-            this.dgvPadre.Size = new System.Drawing.Size(800, 400);
+            this.dgvPadre.RowHeadersWidth = 51;
+            this.dgvPadre.Size = new System.Drawing.Size(1067, 493);
             this.dgvPadre.StandardTab = true;
             this.dgvPadre.TabIndex = 1;
             // 
-            // cbxBuscar
-            // 
-            this.cbxBuscar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cbxBuscar.FormattingEnabled = true;
-            this.cbxBuscar.Location = new System.Drawing.Point(341, 32);
-            this.cbxBuscar.Name = "cbxBuscar";
-            this.cbxBuscar.Size = new System.Drawing.Size(121, 21);
-            this.cbxBuscar.TabIndex = 4;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(341, 13);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Buscar por:";
-            // 
             // FormProcesos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 467);
+            this.ClientSize = new System.Drawing.Size(1067, 575);
             this.Controls.Add(this.dgvPadre);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "FormProcesos";
             this.Text = "FormProcesos";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
@@ -156,7 +166,7 @@
         public System.Windows.Forms.Button btnNuevo;
         public System.Windows.Forms.Button btnModificar;
         public System.Windows.Forms.DataGridView dgvPadre;
-        private System.Windows.Forms.Label label1;
         public System.Windows.Forms.ComboBox cbxBuscar;
+        public System.Windows.Forms.Label label1;
     }
 }
