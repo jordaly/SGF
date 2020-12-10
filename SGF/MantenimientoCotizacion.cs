8a9dbc1fc1d922a4bef2358af34e11f81f81b436
Jordaly José Suriel Vargas <38952588+jordaly@users.noreply.github.com>
2020-12-10 10:03:32 -0400

    Merge branch 'master' into jordaly

---
+++ SGF/MantenimientoCotizacion.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SGF
+{
+    public partial class MantenimientoCotizacion : FormProcesos
+    {
+        public string BuscarDatos = "select c.id,t.id as idCliente, t.nombre,c.fecha_in,s.nombre_sucursal,c.total from cotizacion as c,sucursal as s, tercero as t where t.id=c.idCliente and s.id=c.idSucursal";
+        public MantenimientoCotizacion()
+        {
+            InitializeComponent();
+            cbxBuscar.SelectedIndex = 0;
+            refrescarDatos(BuscarDatos);
+        }
+
+        public override void Borrar()
+        {
+            DialogResult result = MessageBox.Show("Seguro que quiere eliminar la Cotizacion : " + dgvPadre.Rows[dgvPadre.CurrentCell.RowIndex].Cells[1].Value.ToString() + " Codigo: " + dgvPadre.Rows[dgvPadre.CurrentCell.RowIndex].Cells[0].Value.ToString(), "Atención", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                cmd = "delete from cotizacion where id = '" + dgvPadre.Rows[dgvPadre.CurrentCell.RowIndex].Cells[0].Value.ToString() + "';";
+                ds = Utilidades.EjecutarDS(cmd);
+                MessageBox.Show("Se ha eliminado Exitosamente");
+                refrescarDatos(BuscarDatos);
+            }
+            else
+            {
+                // No hacer nada
+            }
+
+        }
+
+        public override void Nuevo()
+        {
+            RegistroSuplidores rc = new RegistroSuplidores();
+            rc.ShowDialog();
+
+
+            refrescarDatos(BuscarDatos);
+        }
+
+        public override void Modificar()
+        {
+            FrmCotizacion rc = new FrmCotizacion();
+            cmd = "select *from detalle_cotizacion where idCotizacion='"+ dgvPadre.Rows[dgvPadre.CurrentCell.RowIndex].Cells[0].Value.ToString() + "'";
+            rc.ShowDialog();
+
+            refrescarDatos(BuscarDatos);
+
+        }
+
+        public string codigo_suplidor = "";
+        public string nombre_suplidor = "";
+        public string RNC = "";
+        public override void Seleccionar()
+        {
+
+            if (dgvPadre.Rows.Count != 0)
+            {
+                codigo_suplidor = dgvPadre.Rows[dgvPadre.CurrentCell.RowIndex].Cells[0].Value.ToString();
+                nombre_suplidor = dgvPadre.Rows[dgvPadre.CurrentCell.RowIndex].Cells[1].Value.ToString();
+
+                this.Close();
+            }
+
+
+
+        }
+
+        public override void Buscar()
+        {
+            FormBarraBusqueda bb = new FormBarraBusqueda();
+            bb.ShowDialog();
+            string parametro = bb.parametro;
+            string v = "";
+            if (cbxBuscar.Text == "id" || cbxBuscar.Text == "estado")
+            {
+                v = "s.";
+            }
+            else
+            {
+                v = "t.";
+            }
+
+
+            cmd = BuscarDatos;
+            //MessageBox.Show("se esta ejecuetando");
+            if (!String.IsNullOrEmpty(parametro.Trim()))
+            {
+                cmd += "and " + v + cbxBuscar.Text + " like('%" + parametro.Trim() + "%')";
+            }
+            ds = Utilidades.EjecutarDS(cmd);
+            //MessageBox.Show(cmd);
+            if (ds.Tables.Count > 0)
+            {
+                dgvPadre.DataSource = ds.Tables[0];
+            }
+        }
+    }
+}
