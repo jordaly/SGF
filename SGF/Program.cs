a752a3e116cc38cba97920dfc082edc98ea361de
alejandritoRD <57777016+alejandritoRD@users.noreply.github.com>
2020-12-11 11:49:53 -0400

    copiaSeguridad-Alejandro,11/12/2020
    aqui se hizo algo

--- SGF/Program.cs
+++ SGF/Program.cs
@@ -16,7 +16,7 @@ namespace SGF
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmCotizacion());
+            Application.Run(new FrmMenu());
 
 
         }
